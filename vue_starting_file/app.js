52af1198af2b62dd8ccc772bb203811467f01ae2
Nathan <HallidayN@outlook.com>
2024-02-28 09:00:29 -0400

    Add files via upload

---
+++ vue_starting_file/app.js
@@ -0,0 +1,5 @@
+// function needed to get random values
+function getRandomValue(min, max) {
+    // this equation give us a value between the min and max value 
+    return Math.floor(Math.random() * (max - min)) + min;
+}
